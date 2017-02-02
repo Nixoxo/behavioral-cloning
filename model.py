f6645f903279e2818c466226423b953dd0941028
nixoxo <nico.axtmann95@gmail.com>
2017-02-02 22:48:54 +0100

    initial commit

---
+++ model.py
@@ -0,0 +1,71 @@
+import pickle
+import numpy as np
+import math
+import tensorflow as t
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.models import model_from_json
+import json
+import os
+
+pickleFile = open('behave.pickle', 'rb')
+data = pickle.load(pickleFile)
+X_features = data['features']
+Y_labels = data['labels']
+from sklearn.utils import shuffle
+features, labels = shuffle(X_features, Y_labels)
+
+model_json = "model.json"
+model_weights = "model.h5"
+height = 32
+width = 64
+model = Sequential()
+# Kernel Size 3 x 3 (16 Filters)
+model.add(Convolution2D(16, 3, 3, input_shape=(height, width, 3), border_mode='valid'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+
+# Kernel Size 3 x 3 (32 Filters)
+model.add(Convolution2D(32, 3, 3, border_mode='valid'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+
+# Kernel Size 3 x 3 (48 Filters)
+model.add(Convolution2D(48, 3, 3, border_mode='valid'))
+model.add(MaxPooling2D((2, 2)))
+
+
+# Kernel Size 3 x 3 (64 Filters)
+#model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+#model.add(MaxPooling2D((2, 2)))
+#model.add(Activation('relu'))
+
+
+model.add(Flatten())
+
+#model.add(Dense(1164))
+#model.add(Activation('relu'))
+
+model.add(Dense(100))
+model.add(Activation('relu'))
+
+model.add(Dense(50))
+model.add(Activation('relu'))
+
+model.add(Dense(1))
+
+model.compile('adam', 'mean_squared_error', ['accuracy'])
+
+history = model.fit(features, labels, nb_epoch=100, validation_split=0.2)
+
+model_as_json = model.to_json()
+try:
+    os.remove(model_json)
+    os.remove(model_weights)
+except OSError:
+    pass
+with open(model_json, 'w') as output:
+    json.dump(model_as_json, output)
+model.save_weights(model_weights)
---
+++ preprocess.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.misc import imresize
+from six.moves import cPickle as pickle
+import cv2
+
+root_data = "data/"
+log_file = root_data + "driving_log.csv"
+pickle_file = "behave.pickle"
+
+
+def extract_features(line):
+    data = line.replace("\n", "").split(",")
+    center = data[0]
+    steering = data[3]
+    throttle = data[4]
+    brake = data[5]
+    speed = data[6]
+    return center, steering, throttle, brake, speed
+
+
+def read_image(line):
+    extracted = extract_features(line)
+    img_path = extracted[0]
+    path = root_data + "" + img_path
+    data = plt.imread(path)
+    label = extracted[1]
+    return data, label
+
+def normalize(x):
+    return (x-127.5)/127.5
+
+def pipeline(img):
+    x = img[45:]
+    x = normalize(x)
+    x = imresize(x, (32, 64))
+    return x
+
+def flip_image(x, y):
+    x = cv2.flip(x, 1)
+    return x, -float(y)
+
+def save_file():
+    with open(log_file, 'r') as reader:
+        lines = reader.readlines()
+        total_size = len(lines)-2
+        X = np.zeros(shape=(2*total_size, 32, 64, 3))
+        Y = np.zeros(shape=(2*total_size))
+        print("Starting to extract data")
+        print("Extracting a total of ", 2*total_size)
+        for i in range(1, total_size):
+            try:
+                x, y = read_image(lines[i])
+                x = pipeline(x)
+                X[i-1] = x
+                Y[i-1] = float(y)
+            except Exception as e:
+                print(e)
+        print("created normal images")
+        for i in range(1, total_size):
+            try:
+                x, y, read_image(lines[i])
+                x, y = flip_image(x, y)
+                X[total_size + i -1] = x
+                Y[total_size + i -1] = y
+            except Exception as e:
+                print(e)
+        print("created flipped images")
+
+
+    print(len(X))
+    print(len(Y))
+    data = {
+        'features': X,
+        'labels': Y
+    }
+
+    try:
+        with open(pickle_file, 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+            print("Data successfully saved")
+    except Exception as e:
+        print("Unable to save: ",e)
+
+#save_file()
